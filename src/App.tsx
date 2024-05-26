8f0dffd72bbd6bcdc3eb9a0e658713d7535c656b
riccardobattiato <rbattiato@outlook.it>
2024-05-26 18:59:47 +0200

    feat(ui): app layout (jsx only)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.scss";
 import { generateMars } from "@core/mars";
+import { Direction, Rotation } from "@core/rover/types";
 import { useMarsRover } from "@hooks/useMarsRover";
-import Obstacle from "@ui/Obstacle";
-import Rover from "@ui/Rover";
+import Controls from "@ui/Controls";
+import Hud from "@ui/Hud";
+import Display from "@ui/Display";
+import Terminal from "@ui/Terminal";
 
 const mars = generateMars(5);
 
@@ -13,9 +16,25 @@ function App() {
     <div className="app">
       <h1 className="app__title">Mars Rover</h1>
       <main className="app__main">
-        <div className="app__mars">Mars</div>
-        <div className="app__terminal">Terminal</div>
-        <div className="app__controls">Controls</div>
+        <div className="app__mars">
+          <Display mars={mars} rover={rover} />
+        </div>
+        <div className="app__terminal">
+          <Terminal data={logs} />
+        </div>
+        <div className="app__hud">
+          <div className="app__hud-display">
+            <Hud {...rover} />
+          </div>
+          <div className="app__hud-controls">
+            <Controls
+              onPressUp={() => handleMove(Direction.FORWARD)}
+              onPressRight={() => handleRotate(Rotation.RIGHT)}
+              onPressDown={() => handleMove(Direction.BACKWARD)}
+              onPressLeft={() => handleRotate(Rotation.LEFT)}
+            />
+          </div>
+        </div>
       </main>
     </div>
   );
