61731d1c1d66c1968a0f57a9a04629d39d0c5727
riccardobattiato <rbattiato@outlook.it>
2024-05-26 20:21:33 +0200

    feat(ui): fully responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -16,23 +16,25 @@ function App() {
     <div className="app">
       <h1 className="app__title">Mars Rover</h1>
       <main className="app__main">
-        <div className="app__mars">
+        <div className="app__display">
           <Display mars={mars} rover={rover} />
         </div>
-        <div className="app__terminal">
-          <Terminal data={logs} />
-        </div>
-        <div className="app__hud">
-          <div className="app__hud-display">
-            <Hud {...rover} />
+        <div className="app__inputs">
+          <div className="app__terminal">
+            <Terminal data={logs} />
           </div>
-          <div className="app__hud-controls">
-            <Controls
-              onPressUp={() => handleMove(Direction.FORWARD)}
-              onPressRight={() => handleRotate(Rotation.RIGHT)}
-              onPressDown={() => handleMove(Direction.BACKWARD)}
-              onPressLeft={() => handleRotate(Rotation.LEFT)}
-            />
+          <div className="app__hud">
+            <div className="app__hud-display">
+              <Hud {...rover} />
+            </div>
+            <div className="app__hud-controls">
+              <Controls
+                onPressUp={() => handleMove(Direction.FORWARD)}
+                onPressRight={() => handleRotate(Rotation.RIGHT)}
+                onPressDown={() => handleMove(Direction.BACKWARD)}
+                onPressLeft={() => handleRotate(Rotation.LEFT)}
+              />
+            </div>
           </div>
         </div>
       </main>
