5aa8ac918da0ed0b9f595bab9806a1a8c68934bc
riccardobattiato <rbattiato@outlook.it>
2024-05-26 11:53:23 +0200

    feat(ui): obstacle

---
+++ src/ui/Obstacle.tsx
@@ -0,0 +1,26 @@
+const Obstacle = () => (
+  <div className="obstacle">
+    <svg
+      className="obstacle__vector"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="7.5 263.5 49 49"
+    >
+      <path
+        fill="none"
+        d="m42.2 279.2 2.95 3-3 3-3-2.95 3.05-3.05M33.75 281.4l4 4-4 4-4-4 4-4M31.2 275.15l2.95-3 3.05 3-3 3-3-3"
+      />
+      <path fill="none" d="m8 288 24-24 24 24-24 24-24-24" />
+      <path
+        fill="none"
+        d="m31.2 275.15 2.95-3 3.05 3-3 3-3-3M33.75 281.4l4 4-4 4-4-4 4-4"
+      />
+      <path
+        fill="none"
+        d="m8 288 8-16 16-8-24 24 24 24 24-24-24-24 16 8 8 16-8 16-16 8-16-8-8-16"
+      />
+      <path fill="none" d="m42.2 279.2 2.95 3-3 3-3-2.95 3.05-3.05" />
+    </svg>
+  </div>
+);
+
+export default Obstacle;
